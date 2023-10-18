a9d124a47299eb075a9ae9f44916f223ca6438ee
tom <lim1017@hotmail.com>
2023-10-18 16:35:54 -0400

    first lanchain interaction

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from langchain.llms import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load variables from .env into os.environ
+
+openAI_key = os.environ.get('openAI_key')
+
+
+llm = OpenAI(openai_api_key=openAI_key)
+
 print('hello main')
+print('loading...')
+result = llm('Write a poem about the sun')
+print(result)
